f0e7fb9378902635a0613ef36f1df19fddd78e06
clarisha octa viany phiranti <57120890+clarishaocta@users.noreply.github.com>
2019-12-02 14:43:47 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$uid="Facebook_451323462190178";
+$raw_uid="7d34e487-69fd-4ce9-9ab8-a0fcf21b8cf3";
+$user_name="Clarisha+Octa";
+$invite_code="F249nlMzGk";
+$user_token="AbzRyPkKK3JZN4NH";
+$own_mac="08%3A8C%3A2C%3ACF%3AE8%3A93";
+$android_id="d32a59f87191ca9f";
+$device_type="samsung++SM-J320G";
+
+
+?>
